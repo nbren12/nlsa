13e4f2d1532844e139a81f730c87a719ad6d09a6
Noah D. Brenowitz <nbren12@gmail.com>
2015-07-10 18:21:50 -0400

    fixed bug in plotting

--- plot_phi.py
+++ plot_phi.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 d = np.load("eigs.pkl")
 phi = d['phi']
- df = pd.DataFrame(phi/phi[:,0][:,None])
-df = pd.DataFrame(phi)
+df = pd.DataFrame(phi/phi[:,0][:,None])
+# df = pd.DataFrame(phi)
 df.ix[:,:20].plot(subplots=True, layout=(-1,3))
 plt.show()
  
